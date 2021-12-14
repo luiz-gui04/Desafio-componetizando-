0b67c4d3693625c0437931cc7bb5af8512694460
luiz-gui04 <luizgui030204@gmail.com>
2021-12-14 11:12:28 -0300

    finalizando desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
+import '../styles/sidebar.scss';
+
+
+interface SideBarProps{
+  switchGenre: (id: number) => void;
+  film: Array<genres>;
+  selectFilmId: Number;
+}
+
+interface genres{
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({switchGenre, film, selectFilmId}: SideBarProps) {
   // Complete aqui
+      
+  
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {film.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => switchGenre(genre.id)}
+          selected={selectFilmId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
